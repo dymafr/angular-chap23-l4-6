9d9bbdb4b0c9108cbfa6421d85be26aa84b5e7db
dymafr <32801166+dymafr@users.noreply.github.com>
2023-09-07 13:18:35 +0200

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -35,6 +35,7 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { CdkTableModule } from '@angular/cdk/table';
 import { DatepickerFilterExample } from './app/datepicker-filter-example';
+import 'zone.js'; // pour Stackblitz uniquement
 
 @NgModule({
   exports: [
